8f66132ce1983168c528cad639060e670be0df7a
Aly <Aly@DESKTOP-AJ5N8FK>
2025-11-01 23:35:41 -0400

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace AnotisApp
+{
+    partial class Anotis
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tituloTexto = new System.Windows.Forms.TextBox();
+            this.notaTexto = new System.Windows.Forms.TextBox();
+            this.tituloLabel = new System.Windows.Forms.Label();
+            this.notaLabel = new System.Windows.Forms.Label();
+            this.historialDeNotas = new System.Windows.Forms.DataGridView();
+            this.btnNuevaNota = new System.Windows.Forms.Button();
+            this.btnCargarNota = new System.Windows.Forms.Button();
+            this.btnGuardarNota = new System.Windows.Forms.Button();
+            this.btnBorrarNota = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.historialDeNotas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tituloTexto
+            // 
+            this.tituloTexto.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tituloTexto.Location = new System.Drawing.Point(12, 33);
+            this.tituloTexto.Name = "tituloTexto";
+            this.tituloTexto.Size = new System.Drawing.Size(410, 20);
+            this.tituloTexto.TabIndex = 0;
+            // 
+            // notaTexto
+            // 
+            this.notaTexto.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.notaTexto.Location = new System.Drawing.Point(12, 72);
+            this.notaTexto.Multiline = true;
+            this.notaTexto.Name = "notaTexto";
+            this.notaTexto.Size = new System.Drawing.Size(410, 366);
+            this.notaTexto.TabIndex = 1;
+            // 
+            // tituloLabel
+            // 
+            this.tituloLabel.Location = new System.Drawing.Point(9, 17);
+            this.tituloLabel.Name = "tituloLabel";
+            this.tituloLabel.Size = new System.Drawing.Size(413, 13);
+            this.tituloLabel.TabIndex = 2;
+            this.tituloLabel.Text = "Titulo:";
+            this.tituloLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // notaLabel
+            // 
+            this.notaLabel.Location = new System.Drawing.Point(9, 56);
+            this.notaLabel.Name = "notaLabel";
+            this.notaLabel.Size = new System.Drawing.Size(413, 13);
+            this.notaLabel.TabIndex = 3;
+            this.notaLabel.Text = "Nota:";
+            this.notaLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // historialDeNotas
+            // 
+            this.historialDeNotas.AllowUserToAddRows = false;
+            this.historialDeNotas.AllowUserToDeleteRows = false;
+            this.historialDeNotas.AllowUserToResizeColumns = false;
+            this.historialDeNotas.AllowUserToResizeRows = false;
+            this.historialDeNotas.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.historialDeNotas.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.historialDeNotas.BackgroundColor = System.Drawing.SystemColors.InactiveBorder;
+            this.historialDeNotas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.historialDeNotas.Location = new System.Drawing.Point(428, 33);
+            this.historialDeNotas.Name = "historialDeNotas";
+            this.historialDeNotas.RowHeadersVisible = false;
+            this.historialDeNotas.Size = new System.Drawing.Size(360, 376);
+            this.historialDeNotas.TabIndex = 4;
+            this.historialDeNotas.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.historialDeNotas_CellDoubleClick);
+            // 
+            // btnNuevaNota
+            // 
+            this.btnNuevaNota.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnNuevaNota.Location = new System.Drawing.Point(428, 415);
+            this.btnNuevaNota.Name = "btnNuevaNota";
+            this.btnNuevaNota.Size = new System.Drawing.Size(75, 23);
+            this.btnNuevaNota.TabIndex = 5;
+            this.btnNuevaNota.Text = "Nueva Nota";
+            this.btnNuevaNota.UseVisualStyleBackColor = true;
+            this.btnNuevaNota.Click += new System.EventHandler(this.btnNuevaNota_Click);
+            // 
+            // btnCargarNota
+            // 
+            this.btnCargarNota.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCargarNota.Location = new System.Drawing.Point(596, 415);
+            this.btnCargarNota.Name = "btnCargarNota";
+            this.btnCargarNota.Size = new System.Drawing.Size(75, 23);
+            this.btnCargarNota.TabIndex = 6;
+            this.btnCargarNota.Text = "Cargar Nota";
+            this.btnCargarNota.UseVisualStyleBackColor = true;
+            this.btnCargarNota.Click += new System.EventHandler(this.btnCargarNota_Click);
+            // 
+            // btnGuardarNota
+            // 
+            this.btnGuardarNota.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGuardarNota.Location = new System.Drawing.Point(509, 415);
+            this.btnGuardarNota.Name = "btnGuardarNota";
+            this.btnGuardarNota.Size = new System.Drawing.Size(81, 23);
+            this.btnGuardarNota.TabIndex = 7;
+            this.btnGuardarNota.Text = "Guardar Nota";
+            this.btnGuardarNota.UseVisualStyleBackColor = true;
+            this.btnGuardarNota.Click += new System.EventHandler(this.btnGuardarNota_Click);
+            // 
+            // btnBorrarNota
+            // 
+            this.btnBorrarNota.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBorrarNota.Location = new System.Drawing.Point(713, 415);
+            this.btnBorrarNota.Name = "btnBorrarNota";
+            this.btnBorrarNota.Size = new System.Drawing.Size(75, 23);
+            this.btnBorrarNota.TabIndex = 8;
+            this.btnBorrarNota.Text = "Borrar Nota";
+            this.btnBorrarNota.UseVisualStyleBackColor = true;
+            this.btnBorrarNota.Click += new System.EventHandler(this.btnBorrarNota_Click);
+            // 
+            // Anotis
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnBorrarNota);
+            this.Controls.Add(this.btnGuardarNota);
+            this.Controls.Add(this.btnCargarNota);
+            this.Controls.Add(this.btnNuevaNota);
+            this.Controls.Add(this.historialDeNotas);
+            this.Controls.Add(this.notaLabel);
+            this.Controls.Add(this.tituloLabel);
+            this.Controls.Add(this.notaTexto);
+            this.Controls.Add(this.tituloTexto);
+            this.Name = "Anotis";
+            this.ShowIcon = false;
+            this.Text = "Anotis";
+            this.Load += new System.EventHandler(this.Anotis_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.historialDeNotas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tituloTexto;
+        private System.Windows.Forms.TextBox notaTexto;
+        private System.Windows.Forms.Label tituloLabel;
+        private System.Windows.Forms.Label notaLabel;
+        private System.Windows.Forms.DataGridView historialDeNotas;
+        private System.Windows.Forms.Button btnNuevaNota;
+        private System.Windows.Forms.Button btnCargarNota;
+        private System.Windows.Forms.Button btnGuardarNota;
+        private System.Windows.Forms.Button btnBorrarNota;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Windows.Forms;
+
+namespace AnotisApp
+{
+    public partial class Anotis : Form
+    {
+        // Constantes para nombres de columnas
+        private const string COL_TITULO = "Titulo";
+        private const string COL_FECHA_CREACION = "Fecha Creacion";
+        private const string COL_CONTENIDO = "Contenido Nota";
+
+        // Tabla en memoria para gestionar las notas
+        private readonly DataTable notas = new DataTable();
+
+        // Indica si estamos editando una nota existente o creando una nueva
+        private bool modoEdicion = false;
+
+        // Carpeta donde se almacenan las notas como archivos .txt
+        private readonly string rutaCarpetaNotas = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+            "AnotisAppNotas");
+
+        public Anotis()
+        {
+            InitializeComponent();
+        }
+
+        private void Anotis_Load(object sender, EventArgs e)
+        {
+            InicializarYCargarNotas();
+        }
+
+        // Inicializa la estructura de la tabla y carga todas las notas desde el disco
+        private void InicializarYCargarNotas()
+        {
+            // Configurar estructura de la tabla si es necesario
+            if (notas.Columns.Count == 0)
+            {
+                notas.Columns.Add(COL_TITULO, typeof(string));
+                notas.Columns.Add(COL_FECHA_CREACION, typeof(DateTime));
+                notas.Columns.Add(COL_CONTENIDO, typeof(string));
+            }
+
+            // Limpiar datos existentes
+            notas.Clear();
+
+            // Crear carpeta si no existe
+            Directory.CreateDirectory(rutaCarpetaNotas);
+
+            // Cargar notas desde archivos
+            CargarNotasDesdeArchivos();
+
+            // Ordenar por fecha (más recientes primero) y vincular al DataGridView
+            DataView vista = notas.DefaultView;
+            vista.Sort = $"{COL_FECHA_CREACION} DESC";
+            historialDeNotas.DataSource = vista;
+
+            // Configurar columnas del DataGridView
+            ConfigurarColumnas();
+        }
+
+        // Configura la apariencia y comportamiento de las columnas del DataGridView
+        private void ConfigurarColumnas()
+        {
+            // Orden visual: Título, Contenido, Fecha
+            historialDeNotas.Columns[COL_TITULO].DisplayIndex = 0;
+            historialDeNotas.Columns[COL_CONTENIDO].DisplayIndex = 1;
+            historialDeNotas.Columns[COL_FECHA_CREACION].DisplayIndex = 2;
+
+            // Hacer todas las columnas de solo lectura
+            foreach (DataGridViewColumn col in historialDeNotas.Columns)
+            {
+                col.ReadOnly = true;
+            }
+
+            // Configurar comportamiento del DataGridView
+            historialDeNotas.RowHeadersVisible = false;
+            historialDeNotas.AllowUserToAddRows = false;
+            historialDeNotas.AllowUserToOrderColumns = false;
+            historialDeNotas.AllowUserToResizeColumns = false;
+            historialDeNotas.AllowUserToResizeRows = false;
+            historialDeNotas.MultiSelect = false;
+        }
+
+        // Lee todos los archivos .txt de la carpeta y los carga en la tabla
+        private void CargarNotasDesdeArchivos()
+        {
+            string[] archivos = Directory.GetFiles(rutaCarpetaNotas, "*.txt");
+
+            foreach (string archivo in archivos)
+            {
+                try
+                {
+                    DataRow fila = notas.NewRow();
+                    fila[COL_TITULO] = Path.GetFileNameWithoutExtension(archivo);
+                    fila[COL_FECHA_CREACION] = File.GetCreationTime(archivo);
+                    fila[COL_CONTENIDO] = File.ReadAllText(archivo);
+                    notas.Rows.Add(fila);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error al cargar {archivo}: {ex.Message}");
+                }
+            }
+        }
+
+        // Limpia los campos para crear una nueva nota
+        private void btnNuevaNota_Click(object sender, EventArgs e)
+        {
+            LimpiarCampos();
+        }
+
+        // Carga la nota seleccionada en los campos de edición
+        private void btnCargarNota_Click(object sender, EventArgs e)
+        {
+            if (!ValidarSeleccion("cargar")) return;
+
+            try
+            {
+                DataRow fila = ObtenerFilaSeleccionada();
+                tituloTexto.Text = fila[COL_TITULO].ToString();
+                notaTexto.Text = fila[COL_CONTENIDO].ToString();
+                modoEdicion = true;
+            }
+            catch (Exception ex)
+            {
+                MostrarError("Error al cargar la nota", ex.Message);
+            }
+        }
+
+        // Guarda la nota actual (nueva o editada) en el disco
+        private void btnGuardarNota_Click(object sender, EventArgs e)
+        {
+            // Validar título
+            if (string.IsNullOrWhiteSpace(tituloTexto.Text))
+            {
+                MessageBox.Show("El título de la nota no puede estar vacío.",
+                    "Título Requerido", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string tituloNuevo = tituloTexto.Text.Trim();
+            string contenido = notaTexto.Text;
+            string archivoNuevo = ObtenerRutaArchivo(tituloNuevo);
+
+            try
+            {
+                // Si estamos editando y el título cambió, eliminar el archivo antiguo
+                if (modoEdicion)
+                {
+                    EliminarArchivoAntiguo(tituloNuevo);
+                }
+
+                // Guardar el archivo
+                File.WriteAllText(archivoNuevo, contenido);
+
+                // Recargar y limpiar
+                InicializarYCargarNotas();
+                LimpiarCampos();
+
+                MessageBox.Show($"Nota '{tituloNuevo}' guardada con éxito.",
+                    "Guardado Exitoso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MostrarError("Error al guardar la nota", ex.Message);
+            }
+        }
+
+        // Elimina la nota seleccionada del disco y de la tabla
+        private void btnBorrarNota_Click(object sender, EventArgs e)
+        {
+            if (!ValidarSeleccion("borrar")) return;
+
+            try
+            {
+                DataRow fila = ObtenerFilaSeleccionada();
+                string titulo = fila[COL_TITULO].ToString();
+                string archivo = ObtenerRutaArchivo(titulo);
+
+                // Eliminar archivo y fila
+                if (File.Exists(archivo))
+                {
+                    File.Delete(archivo);
+                }
+                fila.Delete();
+
+                LimpiarCampos();
+            }
+            catch (Exception ex)
+            {
+                MostrarError("Error al borrar la nota", ex.Message);
+            }
+        }
+
+        // Permite cargar una nota haciendo doble clic en el historial
+        private void historialDeNotas_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            btnCargarNota_Click(sender, e);
+        }
+
+        // ============= MÉTODOS AUXILIARES =============
+
+        // Valida que haya una nota seleccionada en el historial
+        private bool ValidarSeleccion(string accion)
+        {
+            if (historialDeNotas.CurrentRow == null)
+            {
+                MessageBox.Show($"Selecciona una nota válida para {accion}.",
+                    "Selección Requerida", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+            return true;
+        }
+
+        // Obtiene la fila DataRow de la selección actual del DataGridView
+        private DataRow ObtenerFilaSeleccionada()
+        {
+            DataRowView rowView = (DataRowView)historialDeNotas.CurrentRow.DataBoundItem;
+            return rowView.Row;
+        }
+
+        // Genera la ruta completa del archivo basándose en el título
+        private string ObtenerRutaArchivo(string titulo)
+        {
+            string nombreLimpio = string.Join("_", titulo.Split(Path.GetInvalidFileNameChars()));
+            return Path.Combine(rutaCarpetaNotas, $"{nombreLimpio}.txt");
+        }
+
+        // Elimina el archivo antiguo si el título de la nota fue modificado
+        private void EliminarArchivoAntiguo(string tituloNuevo)
+        {
+            if (historialDeNotas.CurrentRow == null) return;
+
+            try
+            {
+                DataRow fila = ObtenerFilaSeleccionada();
+                string tituloAnterior = fila[COL_TITULO].ToString();
+
+                // Solo eliminar si el título cambió
+                if (tituloAnterior != tituloNuevo)
+                {
+                    string archivoAntiguo = ObtenerRutaArchivo(tituloAnterior);
+                    if (File.Exists(archivoAntiguo))
+                    {
+                        File.Delete(archivoAntiguo);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al limpiar archivo anterior: {ex.Message}");
+            }
+        }
+
+        // Limpia los campos de texto y resetea el modo de edición
+        private void LimpiarCampos()
+        {
+            tituloTexto.Text = "";
+            notaTexto.Text = "";
+            modoEdicion = false;
+        }
+
+        // Muestra un mensaje de error al usuario
+        private void MostrarError(string titulo, string mensaje)
+        {
+            MessageBox.Show(mensaje, titulo, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            Console.WriteLine($"{titulo}: {mensaje}");
+        }
+    }
+}
